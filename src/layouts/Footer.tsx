290dac25be88886d9f3a0b94d720fee3d75be565
jkvim <jkvim@outlook.com>
2020-07-09 11:31:59 +0800

    feature: add beian to foooter

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -3,6 +3,13 @@ import './Footer.css'
 
 export default () => {
     return (
-        <div className="footer">2020 jkvim</div>
-    )
+      <div className="footer">
+        <div className="beian">
+          <a rel="nofollow" href="http://www.beian.miit.gov.cn" target="_blank">
+          粤ICP备20052074 
+          </a>
+        </div>
+        <div className="copyright">© 2020 jkvim</div>
+      </div>
+    );
 }
